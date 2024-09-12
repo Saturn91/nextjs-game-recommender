c428110738f1211302b18b79f2727f89f0011366
Manuel G. <manuel.geissberger@gmail.com>
2024-09-12 10:46:08 +0200

    Update page.tsx

--- src/app/create-entry/page.tsx
+++ src/app/create-entry/page.tsx
@@ -1,21 +1,7 @@
-import Banner from "../components/Banner";
-
 export default function CreateEntry() {
   return (
     <div>
       <div>Create Entry</div>
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
     </div>
   );
 }
