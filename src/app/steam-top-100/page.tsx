c22c354280c6eb0eeeeaca255b8637cabfff33e4
Manuel G. <manuel.geissberger@gmail.com>
2024-09-12 15:26:49 +0200

    Revert "try fetching driectly on prod"
    This reverts commit f8aefd218e1e9c3708b396c0421aefdbf0a9c077.

--- src/app/steam-top-100/page.tsx
+++ src/app/steam-top-100/page.tsx
@@ -7,11 +7,7 @@ export default function Steam() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <h2 className="text-center">Last two weeks top 100</h2>
-      <GameList
-        fetchGamesEndpoint={
-          "https://steamspy.com/api.php?request=top100in2weeks"
-        }
-      />
+      <GameList fetchGamesEndpoint={"/api/top-100-steam-games"} />
     </Suspense>
   );
 }
