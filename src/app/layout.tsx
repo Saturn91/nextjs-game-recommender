afa7f0f0c78f77ecbb7a2d13c7c1045f01c314f0
Manuel G. <manuel.geissberger@gmail.com>
2024-09-12 09:24:56 +0200

    adding additional fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import "./globals.css";
-import Container from "@/components/Container";
+import "./styles/globals.css";
+import Container from "@/app/components/Container";
+import { inter, roboto } from "@/app/styles/fonts";
 
 export const metadata: Metadata = {
   title: "Recommened Games",
@@ -14,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"antialiased"}>
+      <body className={`${roboto.className} ${inter.className} antialiased`}>
         <Container>{children}</Container>
       </body>
     </html>
