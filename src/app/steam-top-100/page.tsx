f8aefd218e1e9c3708b396c0421aefdbf0a9c077
Manuel G. <manuel.geissberger@gmail.com>
2024-09-12 15:23:18 +0200

    try fetching driectly on prod

--- src/app/steam-top-100/page.tsx
+++ src/app/steam-top-100/page.tsx
@@ -7,7 +7,11 @@ export default function Steam() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <h2 className="text-center">Last two weeks top 100</h2>
-      <GameList fetchGamesEndpoint={"/api/top-100-steam-games"} />
+      <GameList
+        fetchGamesEndpoint={
+          "https://steamspy.com/api.php?request=top100in2weeks"
+        }
+      />
     </Suspense>
   );
 }
