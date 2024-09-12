808858ea4f308ff681fd15bb4ed4cdbd6db4773a
Manuel G. <manuel.geissberger@gmail.com>
2024-09-12 10:00:36 +0200

    improve mobile title size

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -12,9 +12,7 @@ const Banner: FC = () => (
     />
     <div className="absolute inset-0 bg-black bg-opacity-75"></div>
     <div className="absolute inset-0 flex items-center justify-center">
-      <h1
-        className={`${pixelify_sans.className} text-neongreen text-6xl font-bold`}
-      >
+      <h1 className={`${pixelify_sans.className} text-neongreen `}>
         Recommended Games
       </h1>
     </div>
